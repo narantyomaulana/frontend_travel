5e3febc5a56b0987391738740e44159a60753498
narantyo maulana <olandeoxys@gmail.com>
2022-09-20 11:04:33 +0700

    [add] InputNumber on Detail Page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './assets/scss/style.scss';
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
    <div className="App">
      <Router>
       <Routes>
-        <Route exact path="/" element={<LandingPage />}></Route>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/example" element={<Example />}/>
       </Routes>
     </Router>
    </div>
