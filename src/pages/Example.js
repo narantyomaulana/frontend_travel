516d267cdcd76f32fda7371f840f6e035f1669be
narantyo maulana <olandeoxys@gmail.com>
2022-09-27 10:12:35 +0700

    [Add] Input Date + Utils Format Date

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
-import InputNumber from "elements/Form/InputNumber";
+import { InputDate } from "elements/Form";
 
 export default class Example extends Component {
   state = {
-    value: "1"
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
   };
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -17,14 +21,20 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
+          <InputDate
+              max={30}
+              onChange={this.handleChange}
+              name="value"
+              value={this.state.value}
+            />
+            {/* <InputNumber
               max={30}
               suffix= " night"
               isSuffixPlural
               onChange={this.handleChange}
               name="value"
               value={this.state.value}
-            />
+            /> */}
           </div>
         </div>
       </div>
